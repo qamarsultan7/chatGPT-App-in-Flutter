1127ba0f8d241b6823ce26f8405a524d2de127cc
QaMaR SulTaN <139233554+DEVQamaRSulTaN@users.noreply.github.com>
2024-01-14 17:46:11 +0500

    constants.dart

--- lib/Resources/constants.dart
+++ lib/Resources/constants.dart
@@ -1,7 +1,7 @@
 
 class ApiUrls {
   static var baseUrl='https://api.openai.com/v1/';
-  static var apikey = 'sk-J1kwGPF63';
+  static var apikey = '';
   static var gptModelUrl = '$baseUrl/models';
   static var gptResponseUrl = '$baseUrl/chat/completions';
 }
